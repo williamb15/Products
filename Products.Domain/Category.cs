19ae8b45e139b3f963a2addb0419c3f817eab788
williamb.a <williamb.a@hotmail.com>
2017-09-08 14:31:12 -0500

    video 17 ok, se debe instalar sql para seguir

---
+++ Products.Domain/Category.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Products.Domain
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [MaxLength(50, ErrorMessage ="The field {0} only can contain {1} characters lenght.")]
+        public string Description { get; set; }
+    }
+}
